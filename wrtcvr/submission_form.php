e726fecfb86de1f62670e18ebdde7e43a76c6cd3
Vertmo <basile.pesin@gmail.com>
2017-06-29 14:34:29 +0200

    Removed locallib.php

--- wrtcvr/submission_form.php
+++ wrtcvr/submission_form.php
@@ -10,7 +10,6 @@ defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
 
 require_once($CFG->libdir . '/formslib.php');
-require_once($CFG->dirroot . '/mod/wrtcvr/locallib.php');
 
 /**
  * wrtcvr submission form
